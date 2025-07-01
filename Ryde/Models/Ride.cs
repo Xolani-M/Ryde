6238ff17319af74a5172f24f2146f73eab238c64
Xolani <nathanielmvana@gmail.com>
2025-07-01 10:57:37 +0200

    Added Ride class with neccessary props and constructor

---
+++ Ryde/Models/Ride.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ryde;
+
+namespace Ryde
+{
+    public class Ride
+    {
+        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public int? DriverId { get; set; } // Nullable - might not have driver yet
+        public string PickupLocation { get; set; }
+        public string DropOffLocation { get; set; }
+        public decimal Fare { get; set; }
+        public RideStatus Status { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public DateTime? AcceptedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public double DistanceKm { get; set; }
+
+
+        public Ride()
+        {
+            RequestedAt = DateTime.Now;
+            Status = RideStatus.Requested;
+        }
+
+        public void CalculateFare()
+        {
+            // Simple fare calculation: base fare + distance-based fare
+            decimal baseFare = 5.00m;
+            decimal perKmRate = 2.50m;
+
+            Fare = baseFare + ((decimal)DistanceKm * perKmRate);
+        }
+
+    }
+}

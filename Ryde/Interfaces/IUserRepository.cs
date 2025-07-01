fbf1a533f645edad8ad9d634cf37be1a3f55cd3b
Xolani <nathanielmvana@gmail.com>
2025-07-01 14:54:23 +0200

    added interface for UserRepo

---
+++ Ryde/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryde.Interfaces
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        User GetUserById(int id);
+        User GetUserByUsername(string username);
+        List<User> GetAllUsers();
+        void UpdateUser(User user);
+        bool DeleteUser(int id);
+    }
+}

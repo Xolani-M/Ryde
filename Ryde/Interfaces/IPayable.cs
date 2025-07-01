b36b84dd129c7850b4c20698762e6f4270c0b5e8
Xolani <nathanielmvana@gmail.com>
2025-07-01 14:58:22 +0200

    added interface for payable

---
+++ Ryde/Interfaces/IPayable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryde.Interfaces
+{
+    public interface IPayable
+    {
+        bool ProcessPayment(decimal amount);
+        decimal GetBalance();
+        void AddFunds(decimal amount);
+        void UpdateBalance(decimal amount);
+    }
+}

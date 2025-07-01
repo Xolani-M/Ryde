b3de62ff017faae2bbfff4b213353f2cf0c68952
Xolani <nathanielmvana@gmail.com>
2025-07-01 14:55:26 +0200

    added interface for rideRepo

---
+++ Ryde/Interfaces/IRideRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ryde;
+
+namespace Ryde.Interfaces
+{
+    public interface IRideRepository
+    {
+        void AddRide(Ride ride);
+        Ride GetRideById(int id);
+        List<Ride> GetRidesByPassengerId(int passengerId);
+        List<Ride> GetRidesByDriverId(int driverId);
+        List<Ride> GetAvailableRides();
+        void UpdateRide(Ride ride);
+    }
+}
